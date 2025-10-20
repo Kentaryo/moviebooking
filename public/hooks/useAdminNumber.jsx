ad9ddd80a001e3aa3563243df13180582d728fa0
Kentaryo <khennedymamba@gmail.com>
2025-10-20 23:13:06 +0800

    Delete public/hooks directory

--- public/hooks/useAdminNumber.jsx
+++
@@ -1,7 +0,0 @@
-export function useAdminNumber() {
-  const adminGCashNumber = "09171234567";
-  const adminPayPal = "admin@paypal.com";
-  const adminCreditCard = "4111 1111 1111 1111";
-
-  return { adminGCashNumber, adminPayPal, adminCreditCard };
-}
--- public/hooks/usePaymentModal.jsx
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-
-export function usePaymentModal() {
-  const [visible, setVisible] = useState(false);
-  const open = () => setVisible(true);
-  const close = () => setVisible(false);
-
-  return { visible, open, close };
-}
